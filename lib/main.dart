8d30f200c23ad1a55456b14dc010dfdafdda9aa4
Wendell Neander <neanderdev@gmail.com>
2018-08-15 14:15:23 -0300

    Contador de Pessoas

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: "Contador de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Pessoas: 0",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            )
+        ),
+        Text("Pode Entrar!",
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0
+            )
+        )
+      ],
+    ),
+  ));
+}
