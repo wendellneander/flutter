5e08bfa50ef71dc561ec7884486485e31c58cbb5
Wendell Neander <neanderdev@gmail.com>
2018-08-17 13:59:34 -0300

    finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,38 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Stack(
+    home: Home()
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode entrar!";
+
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+
+      if(_people < 0){
+        _infoText = "HAHAHA";
+        _people = 0;
+      }else if(_people <= 10){
+        _infoText = "Pode entrar!";
+      }else{
+        _infoText = "Lotado";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
         Image.asset(
           'images/casa.jpeg',
@@ -13,7 +44,7 @@ void main(){
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Pessoas: 0",
+            Text("Pessoas: $_people",
                 style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold
@@ -26,19 +57,23 @@ void main(){
                     padding: EdgeInsets.all(10.0),
                     child: FlatButton(
                       child: Text('+1', style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                      onPressed: () {},
+                      onPressed: () {
+                        _changePeople(1);
+                      },
                     )
                 ),
                 Padding(
                     padding: EdgeInsets.all(10.0),
                     child: FlatButton(
                       child: Text('-1', style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                      onPressed: () {},
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
                     )
                 )
               ],
             ),
-            Text("Pode Entrar!",
+            Text(_infoText,
                 style: TextStyle(
                     color: Colors.white,
                     fontStyle: FontStyle.italic,
@@ -48,6 +83,6 @@ void main(){
           ],
         ),
       ],
-    )
-  ));
+    );
+  }
 }
