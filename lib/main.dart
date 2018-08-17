307367a41ea97710915177928bda233372654b9b
Wendell Neander <neanderdev@gmail.com>
2018-08-17 13:49:26 -0300

    finalizando layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Stack(
       children: <Widget>[
-        Text("Pessoas: 0",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-            )
+        Image.asset(
+          'images/casa.jpeg',
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Text("Pode Entrar!",
-            style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: 0",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text('+1', style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    )
+                ),
+                Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text('-1', style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    )
+                )
+              ],
+            ),
+            Text("Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0
+                )
             )
-        )
+          ],
+        ),
       ],
-    ),
+    )
   ));
 }
